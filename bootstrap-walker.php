735920d4fe773f2b5e50f777e4476626eba471ee
deshack <deshack@linux.it>
2013-09-03 20:35:41 +0200

    Don't show caret if this is the last depth level

--- bootstrap-walker.php
+++ bootstrap-walker.php
@@ -76,7 +76,7 @@ class Bootstrap_Walker extends Walker_Nav_Menu{
 			$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
 
 			//If item has_children add atts to a
-			if($args->has_children) {
+			if($args->has_children && $depth === 0) {
 				$atts['href']   		= '#';
 				$atts['data-toggle']	= 'dropdown';
 				$atts['class']			= 'dropdown-toggle';
@@ -111,7 +111,7 @@ class Bootstrap_Walker extends Walker_Nav_Menu{
 			}
 			
 			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-			$item_output .= ($args->has_children) ? ' <span class="caret"></span></a>' : '</a>';
+			$item_output .= ($args->has_children && $depth === 0) ? ' <span class="caret"></span></a>' : '</a>';
 			$item_output .= $args->after;
 
 			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
