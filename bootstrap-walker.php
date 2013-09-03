ee61ce64751e3c866d69a2938815db7c327cdc02
deshack <deshack@linux.it>
2013-09-03 09:50:53 +0200

    Update Boostrap Walker

--- bootstrap-walker.php
+++ bootstrap-walker.php
@@ -1,124 +1,157 @@
 <?php
 /**
- * Bootstrap_Walker for Wordpress
- * Author: George Huger, Illuminati Karate, Inc
- * More Info: http://illuminatikarate.com/blog/bootstrap-walker-for-wordpress
- *
- * Formats a Wordpress menu to be used as a Bootstrap dropdown menu (http://getbootstrap.com).
- *
- * Specifically, it makes these changes to the normal Wordpress menu output to support Bootstrap:
- * 	- adds a 'dropdown' class to level-0 <li>'s which contain a dropdown
- * 	- adds a 'dropdown-submenu' class to level-1 <li>'s which contain a dropdown
- * 	- adds the 'dropdown-menu' class to level-1 and level-2 <ul>'s
- *
- * Supports menus up to 3 levels deep.
- * 
+ * Class Name: wp_bootstrap_navwalker
+ * GitHub URI: https://github.com/twittem/wp-bootstrap-navwalker
+ * Description: A custom WordPress nav walker class to implement the Twitter Bootstrap 3.0 navigation style in a custom theme using the WordPress built in menu manager.
+ * Version: 3.0.0
+ * Author: Edward McIntyre - @twittem
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 class Bootstrap_Walker extends Walker_Nav_Menu{
+	
 	/**
-	 * Start of the <ul>
+	 * @see Walker::start_lvl()
+	 * @since 3.0.0
 	 *
-	 * Note on $depth: Counterintuitively, $depth here means the "depth right before we start this menu". 
-	 * 		So basically add one to what you'd expect it to be
-	 */        
-	function start_lvl( &$output, $depth = 0, $args = array() ){
-		$tabs = str_repeat("\t", $depth);
-		// If we are about to start the first submenu, we need to give it a dropdown-menu class
-		if ($depth == 0 || $depth == 1) { //really, level-1 or level-2, because $depth is misleading here (see note above)
-			$output .= "\n{$tabs}<ul class=\"dropdown-menu\">\n";
-		} else {
-			$output .= "\n{$tabs}<ul>\n";
-		}
-		return;
-	}
-
-	/**
-	 * End of the <ul>
-	 *
-	 * Note on $depth: Counterintuitively, $depth here means the "depth right before we start this menu". 
-	 * 		So basically add one to what you'd expect it to be
-	 */        
-	function end_lvl( &$output, $depth = 0, $args = array() ){
-		if ($depth == 0) { // This is actually the end of the level-1 submenu ($depth is misleading here too!)
-			// we don't have anything special for Bootstrap, so we'll just leave an HTML comment for now
-			$output .= '<!--.dropdown-->';
-		}
-		$tabs = str_repeat("\t", $depth);
-		$output .= "\n{$tabs}</ul>\n";
-		return;
-	}
-        
-	/**
-	 * Output the <li> and the containing <a>
-	 * Note: $depth is "correct" at this level
-	 */        
-	function start_el( &$output, $object, $depth = 0, $args = array(), $current_object_id = 0 ){
-		global $wp_query;
-		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
-		$class_names = $value = '';
-		$classes = empty( $object->classes ) ? array() : (array) $object->classes;
-
-		/* If this item has a dropdown menu, add the 'dropdown' class for Bootstrap */
-		if ($object->hasChildren) {
-			$classes[] = 'dropdown';
-			// level-1 menus also need the 'dropdown-submenu' class
-			if($depth == 1) {
-				$classes[] = 'dropdown-submenu';
-			}
-		}
-
-		/* This is the stock Wordpress code that builds the <li> with all of its attributes */
-		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $object ) );
-		$class_names = ' class="' . esc_attr( $class_names ) . '"';
-		$output .= $indent . '<li id="menu-item-'. $object->ID . '"' . $value . $class_names .'>';            
-		$attributes  = ! empty( $object->attr_title ) ? ' title="'  . esc_attr( $object->attr_title ) .'"' : '';
-		$attributes .= ! empty( $object->target )     ? ' target="' . esc_attr( $object->target     ) .'"' : '';
-		$attributes .= ! empty( $object->xfn )        ? ' rel="'    . esc_attr( $object->xfn        ) .'"' : '';
-		$attributes .= ! empty( $object->url )        ? ' href="'   . esc_attr( $object->url        ) .'"' : '';
-		$object_output = isset( $args->before ) ? $args->before : '';
-        
-		/* If this item has a dropdown menu, make clicking on this link toggle it */
-		if ($object->hasChildren && $depth == 0) {
-			$object_output .= '<a'. $attributes .' class="dropdown-toggle" data-toggle="dropdown">';
-		} else {
-			$object_output .= '<a'. $attributes .'>';
-		}
-
-		$object_output .= ( isset( $args->link_before ) ? $args->link_before : '' ) . apply_filters( 'the_title', $object->title, $object->ID ) . ( isset( $args->link_after ) ? $args->link_after : '' );
-
-		/* Output the actual caret for the user to click on to toggle the menu */            
-		if ($object->hasChildren && $depth == 0) {
-			$object_output .= ' <b class="caret"></b></a>';
-		} else {
-			$object_output .= '</a>';
-		}
-
-		$object_output .= isset( $args->after ) ? $args->after : '';
-		$output .= apply_filters( 'walker_nav_menu_start_el', $object_output, $object, $depth, $args );
-		return;
-	}
-
-	/**
-	 * Close the <li>
-	 * Note: the <a> is already closed
-	 * Note 2: $depth is "correct" at this level
-	 */        
-	function end_el ( &$output, $object, $depth = 0, $args = array() ){
-		$output .= '</li>';
-		return;
-	}
-
-	/**
-	 * Add a 'hasChildren' property to the item
-	 * Code from: http://wordpress.org/support/topic/how-do-i-know-if-a-menu-item-has-children-or-is-a-leaf#post-3139633 
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int $depth Depth of page. Used for padding.
 	 */
-	function display_element ($element, &$children_elements, $max_depth, $depth = 0, $args, &$output){
-		// check whether this item has children, and set $item->hasChildren accordingly
-		$element->hasChildren = isset($children_elements[$element->ID]) && !empty($children_elements[$element->ID]);
-
-		// continue with normal behavior
-		return parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+	function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul role=\"menu\" class=\" dropdown-menu\">\n";
 	}
+
+	/**
+	 * @see Walker::start_el()
+	 * @since 3.0.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param object $item Menu item data object.
+	 * @param int $depth Depth of menu item. Used for padding.
+	 * @param int $current_page Menu item ID.
+	 * @param object $args
+	 */
+
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+		/**
+		 * Dividers, Headers or Disabled
+	     * =============================
+		 * Determine whether the item is a Divider, Header, Disabled or regular
+		 * menu item. To prevent errors we use the strcasecmp() function to so a
+		 * comparison that is not case sensitive. The strcasecmp() function returns
+		 * a 0 if the strings are equal.
+		 */
+		if (strcasecmp($item->attr_title, 'divider') == 0 && $depth === 1) {
+			$output .= $indent . '<li role="presentation" class="divider">';
+		} else if (strcasecmp($item->attr_title, 'dropdown-header') == 0 && $depth === 1) {
+			$output .= $indent . '<li role="presentation" class="dropdown-header">' . esc_attr( $item->title );
+		} else if (strcasecmp($item->attr_title, 'disabled') == 0) {
+			$output .= $indent . '<li role="presentation" class="disabled"><a href="#">' . esc_attr( $item->title ) . '</a>';
+		} else {
+
+			$class_names = $value = '';
+
+			$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+			$classes[] = 'menu-item-' . $item->ID;
+
+			$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+			
+			if($args->has_children) {	$class_names .= ' dropdown'; }
+			if(in_array('current-menu-item', $classes)) { $class_names .= ' active'; }
+
+			$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+			$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+			$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+			$output .= $indent . '<li' . $id . $value . $class_names .'>';
+
+			$atts = array();
+			$atts['title']  = ! empty( $item->title ) 	   ? $item->title 	   : '';
+			$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+			$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+
+			//If item has_children add atts to a
+			if($args->has_children) {
+				$atts['href']   		= '#';
+				$atts['data-toggle']	= 'dropdown';
+				$atts['class']			= 'dropdown-toggle';
+			} else {
+				$atts['href'] = ! empty( $item->url ) ? $item->url : '';
+			}
+
+			$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+			$attributes = '';
+			foreach ( $atts as $attr => $value ) {
+				if ( ! empty( $value ) ) {
+					$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+					$attributes .= ' ' . $attr . '="' . $value . '"';
+				}
+			}
+
+			$item_output = $args->before;
+
+			/*
+			 * Glyphicons
+			 * ===========
+			 * Since the the menu item is NOT a Divider or Header we check the see
+			 * if there is a value in the attr_title property. If the attr_title
+			 * property is NOT null we apply it as the class name for the glyphicon.
+			 */
+
+			if(! empty( $item->attr_title )){
+				$item_output .= '<a'. $attributes .'><span class="glyphicon ' . esc_attr( $item->attr_title ) . '"></span>&nbsp;';
+			} else {
+				$item_output .= '<a'. $attributes .'>';
+			}
+			
+			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+			$item_output .= ($args->has_children) ? ' <span class="caret"></span></a>' : '</a>';
+			$item_output .= $args->after;
+
+			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+		}
+	}
+
+	/**
+	 * Traverse elements to create list from elements.
+	 *
+	 * Display one element if the element doesn't have any children otherwise,
+	 * display the element and its children. Will only traverse up to the max
+	 * depth and no ignore elements under that depth. 
+	 *
+	 * This method shouldn't be called directly, use the walk() method instead.
+	 *
+	 * @see Walker::start_el()
+	 * @since 2.5.0
+	 *
+	 * @param object $element Data object
+	 * @param array $children_elements List of elements to continue traversing.
+	 * @param int $max_depth Max depth to traverse.
+	 * @param int $depth Depth of current element.
+	 * @param array $args
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @return null Null on failure with no changes to parameters.
+	 */
+
+	function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
+        if ( !$element ) {
+            return;
+        }
+
+        $id_field = $this->db_fields['id'];
+
+        //display this element
+        if ( is_object( $args[0] ) ) {
+           $args[0]->has_children = ! empty( $children_elements[$element->$id_field] );
+        }
+
+        parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+    }
 }
 ?>
